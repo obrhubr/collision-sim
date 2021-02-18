4f0807daac83518923bd65a6ec09b94643a23c15
DerGoyote <geoffrey.karnbach@gmail.com>
2021-02-18 17:06:14 +0100

    Update

--- input.py
+++ input.py
@@ -5,6 +5,15 @@ def draw_borders():
     for loop in borders:
         canvas.create_line(int(loop[0]),int(loop[1]),int(loop[2]),int(loop[3]))
 
+def update_UI():
+    global currentID
+    print(currentID)
+    currentID += 1
+    if currentID < totalTime:
+        window.after(int(timeStep*1000), update_UI)
+    else:
+        window.destroy()
+
 window = Tk()
 window.title("Animation")
 window.state('zoomed')
@@ -21,6 +30,7 @@ objectData = []
 mouvementData = []
 
 currentID = 0
+objectsUI = []
 
 # Reading all informations from data.setup file
 with open(setup_file,"r") as f:
@@ -49,5 +59,9 @@ canvas = Canvas(window, width = 1000, height = 1000, bg = "white")
 canvas.grid(column = 0, row = 0, padx = 10, pady = 10)
 
 draw_borders()
+
+for loop in range(objectNb):
+    objectsUI.append(canvas.create_oval)
+window.after(int(timeStep*1000), update_UI)
 
 window.mainloop()
